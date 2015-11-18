a8f3bdcdebeaa0d1567a1ea5616fbbfd6dca9cdc
insano10 <jdommett@gmail.com>
2015-11-18 08:52:02 +0000

    implement PermMissingElement

---
+++ src/main/java/com/insano10/puzzlers/puzzles/codility/PermMissingElement.java
@@ -0,0 +1,17 @@
+package com.insano10.puzzlers.puzzles.codility;
+
+public class PermMissingElement
+{
+    public int solution(int[] input)
+    {
+        int completeValue = input.length + input.length+1;
+        int actualValue = 0;
+
+        for(int i=0 ; i<input.length ; i++)
+        {
+            actualValue += input[i];
+            completeValue += i;
+        }
+        return completeValue - actualValue;
+    }
+}
---
+++ src/test/java/com/insano10/puzzlers/puzzles/codility/PermMissingElementTest.java
@@ -0,0 +1,19 @@
+package com.insano10.puzzlers.puzzles.codility;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PermMissingElementTest
+{
+
+    @Test
+    public void shouldFindTheMissingElement() throws Exception
+    {
+        int[] input = new int[] {2, 3, 1, 5};
+
+        PermMissingElement permMissingElement = new PermMissingElement();
+
+        assertThat(permMissingElement.solution(input)).isEqualTo(4);
+    }
+}
